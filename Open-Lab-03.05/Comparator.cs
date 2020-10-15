4e6f4162dc6577b55b274072dd89be9da3e15121
pavolhodas <pavolhodas4@gmail.com>
2020-10-15 14:59:57 +0200

    Find out size of string regardless of their case

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_03._05
     {
         public bool MatchCaseInsensitive(string str1, string str2)
         {
-            throw new NotImplementedException();
+
+
+            if (string.Compare(str1, str2, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        
         }
     }
 }
